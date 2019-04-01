1546c9d6c4f7f198e9a21c19fdb3002002f57554
Stephanie Patton <31425878+StephanieRP@users.noreply.github.com>
2019-04-01 16:27:43 -0500

    completed functionality of app

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const MovieCard = props => {
-  return;
+const MovieCard = ({ movie }) => {
+  const { title, director, metascore, stars, id } = movie;
+  return (
+    <div className="movie-details">
+      <NavLink to={`/movies/${id}`}>{title}</NavLink>{" "}
+      <div className="movie-director">
+        Director: <em>{director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{metascore}</strong>
+      </div>
+      <h3>Actors</h3>
+      {stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieCard;
